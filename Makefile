795fc8ecad8c8de0f8a77443384341f37cd18256
Ricardo Rodriguez <ricardo@svarmo.com>
2017-09-13 21:18:55 -0700

    Initial OLED implementation, Adafruit not working

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
-BOARD_TAG = uno
-#BOARD_TAG = pro
-#BOARD_SUB = 8MHzatmega328
+#BOARD_TAG = micro
+#BOARD_TAG = uno
+BOARD_TAG = pro
+BOARD_SUB = 8MHzatmega328
 
-ARDUINO_LIBS =
+ARDUINO_LIBS = Wire SPI Adafruit_GFX Adafruit_SSD1306
 ARDUINO_VERSION = 166
 ARDUINO_DIR = ../../env/Arduino
 ARDMK_DIR = ../..//env/ArduinoMake
--- src/Main.cpp
+++ src/Main.cpp
@@ -5,9 +5,38 @@
 // 0x3C
 
 #include "Arduino.h"
+#include <SPI.h>
 #include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 #define BAUD_RATE 9600
+#define OLED_RESET 4
+#define LOGO16_GLCD_HEIGHT 16
+#define LOGO16_GLCD_WIDTH  16
+
+Adafruit_SSD1306 display;
+static const unsigned char PROGMEM logo16_glcd_bmp[] =
+{ B00000000, B11000000,
+  B00000001, B11000000,
+  B00000001, B11000000,
+  B00000011, B11100000,
+  B11110011, B11100000,
+  B11111110, B11111000,
+  B01111110, B11111111,
+  B00110011, B10011111,
+  B00011111, B11111100,
+  B00001101, B01110000,
+  B00011011, B10100000,
+  B00111111, B11100000,
+  B00111111, B11110000,
+  B01111100, B11110000,
+  B01110000, B01110000,
+  B00000000, B00110000 };
+
+#if (SSD1306_LCDHEIGHT != 32)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
 
 byte getOledScreenAddress() {
     byte address = 0x00;
@@ -26,10 +55,60 @@ byte getOledScreenAddress() {
     return 0x00;
 }
 
+void setupOledScreen(byte address) {
+    Serial.println("Screen sequence");
+
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+    display.display();
+    delay(2000);
+    display.clearDisplay();
+
+    // draw a single pixel
+  display.drawPixel(10, 10, WHITE);
+  // Show the display buffer on the hardware.
+  // NOTE: You _must_ call display after making any drawing commands
+  // to make them visible on the display hardware!
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+  // draw a white circle, 10 pixel radius
+  display.fillCircle(display.width()/2, display.height()/2, 10, WHITE);
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+  // text display tests
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println("Hello, world!");
+  display.setTextColor(BLACK, WHITE); // 'inverted' text
+  display.println(3.141592);
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.print("0x"); display.println(0xDEADBEEF, HEX);
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+  // invert the display
+  display.invertDisplay(true);
+  delay(1000);
+  display.invertDisplay(false);
+  delay(1000);
+  display.clearDisplay();
+
+  // draw a bitmap icon and 'animate' movement
+  // testdrawbitmap(logo16_glcd_bmp, LOGO16_GLCD_HEIGHT, LOGO16_GLCD_WIDTH);
+}
+
 void setup() {
     Serial.begin (BAUD_RATE);
-    Wire.begin();
+    Serial.println("Starting");
+    // Wire.begin();
     byte screenAddress = getOledScreenAddress();
+    setupOledScreen(screenAddress);
 
     Serial.println("Started");
 }
